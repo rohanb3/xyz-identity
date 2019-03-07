8f3fe07e9bc337f7d17c5a154ab1cf5b0b98915f
yuriipl <yurii.platonov@ardas.biz>
2019-03-07 19:06:33 +0200

    mock answer

--- src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/CPUsersController.cs
+++ src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/CPUsersController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Xyzies.SSO.Identity.Data.Core;
 using Xyzies.SSO.Identity.Data.Helpers;
+using Xyzies.SSO.Identity.Services.Models.User;
 using Xyzies.SSO.Identity.Services.Service;
 
 namespace Xyzies.SSO.Identity.API.Controllers
@@ -24,16 +26,32 @@ namespace Xyzies.SSO.Identity.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllUsers([FromQuery]LazyLoadParameters lazyLoad = null)
         {
-            var role = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.RoleClaimType)?.Value;
-            var companyId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.CompanyIdPropertyName)?.Value;
-            var userId = int.Parse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.UserIdPropertyName)?.Value);
+            //var role = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.RoleClaimType)?.Value;
+            //var companyId = HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.CompanyIdPropertyName)?.Value;
+            //var userId = int.Parse(HttpContext.User.Claims.FirstOrDefault(x => x.Type == Consts.UserIdPropertyName)?.Value);
 
-            var users = await _users.GetAllCpUsers(userId, role, companyId, lazyLoad);
-            if (users == null)
+            //var users = await _users.GetAllCpUsers(userId, role, companyId, lazyLoad);
+            //if (users == null)
+            //{
+            //    return new ContentResult { StatusCode = 403 };
+            //}
+            return Ok(new LazyLoadedResult<CpUser>()
             {
-                return new ContentResult { StatusCode = 403 };
-            }
-            return Ok(users);
+                Total = 1,
+                Result = new List<CpUser>() {
+                new CpUser { Address = "address",
+                    City = "city",
+                    CompanyId = 7929,
+                    Id = 1,
+                    Email = "email@email.email",
+                    IsActive = true,
+                    LastName = "lastName",
+                    Name = "Name",
+                    Phone = "046846846",
+                    Role = "SalesRep",
+                    IsDeleted = false,
+                    State = "state" } }
+            });
         }
 
         [HttpGet("{id}")]
