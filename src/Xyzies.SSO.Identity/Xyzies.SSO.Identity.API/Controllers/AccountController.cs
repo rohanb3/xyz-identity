351de9af16fab90f442aca308a5e218b26763999
yuriipl <yurii.platonov@ardas.biz>
2019-03-12 18:38:57 +0200

    test

--- src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/AccountController.cs
+++ src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/AccountController.cs
@@ -20,14 +20,14 @@ namespace Xyzies.SSO.Identity.API.Controllers
         [Route("token")]
         public IActionResult GetToken(string email, string password)
         {
-            return Ok("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE1NTIzODM4ODcsIm5iZiI6MTU1MjI5NzQ4NywidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9hcmRhc2Rldi5iMmNsb2dpbi5jb20vZjEwMThhYTktOGY1NC00OTk5LThmMjQtNmU1NWI0Njk1YmIwL3YyLjAvIiwic3ViIjoiNzVjZjBiM2QtOGNlZC00ZDJlLWI1NDgtOGM0MGZhYmU3M2UyIiwiYXVkIjoiZWFmYmU1ODgtMDU4Mi00ODgwLTk2MzUtNmVkYzdjYzhkNzk4Iiwibm9uY2UiOiJkZWZhdWx0Tm9uY2UiLCJpYXQiOjE1NTIyOTc0ODcsImF1dGhfdGltZSI6MTU1MjI5NzQ4Nywib2lkIjoiNzVjZjBiM2QtOGNlZC00ZDJlLWI1NDgtOGM0MGZhYmU3M2UyIiwiZXh0ZW5zaW9uX0dyb3VwIjoiU2FsZXNSZXAiLCJ0ZnAiOiJCMkNfMV9TaUluIn0.OR6Ja8OwB5HCw-rzK8tdfUWHRCfbYa-Hf249KFudG_YaRRFl-c9t1iraFLnsChgTpF0mYhHWnSubm5RrouZTFsWuMFyaFMtAEFHJutKjM-frEttZyQuW5Ei_7xf5nBMTsTvsuP6BN1rCDD8yPKyKqqlVZ8BOEhxd9BQsb1CsnVSQOlHkMZ6i8ZBTzKtOxBcGv26WPAJuam3C8xRBJ_7DZV48nVtco4I1WByVRL48dhrB57T2vqjHfsRaodHnGixZZIQPf08J69qFl2jBF3eP8_Xt_QdhXzmukuX9M3EX5YblJFBquJYXFHMoWy7iq8RuluHqbZX7FmgD08nInpvIaw");
+            return Redirect("https://google.com");
         }
 
         [HttpGet]
         [Route("info")]
         public IActionResult GetInfo(string email, string password)
         {
-            return Ok(new CpUser { Email = "blabla@gmail.com", Id = 1, Name = "Name" });
+            return Redirect("https://translate.google.com/");
         }
 
         [HttpGet]
@@ -51,7 +51,7 @@ namespace Xyzies.SSO.Identity.API.Controllers
         public IActionResult Login([FromQuery]AuthorizeModel authorizeModel)
         {
         //http://localhost:8081/api/account/.well-known/openid-configuration
-            return Redirect("https://google.com");
+            return Redirect("https://ardasdev.b2clogin.com/ardasdev.onmicrosoft.com/oauth2/authresp");
         }
 
         [HttpPost]
