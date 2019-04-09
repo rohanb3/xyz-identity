70ea2572848b19ba04163f8ed87e91ea53652f69
yuriipl <yurii.platonov@ardas.biz>
2019-04-09 17:48:46 +0300

    add getting users for trusted service

--- src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.Data/Helpers/Consts.cs
+++ src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.Data/Helpers/Consts.cs
@@ -81,5 +81,10 @@
             public const string DisablePasswordExpiration = "DisablePasswordExpiration";
             public const string DisablePasswordString = "DisableStrongPassword";
         }
+
+        public class Security
+        {
+            public const string StaticToken = "d64ded6fd8db3fead6c90e600d85cccc02cd3e2dafcc29e8d1ade61263229d0b16b5a92ffa1bad1d6325e302461c7a69630c5c913ab47fb7e284dcabba1ac91e";
+        }
     }
 }
