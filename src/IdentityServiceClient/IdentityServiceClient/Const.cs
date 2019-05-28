9d80fc20c23242a7d458dbe73e7409fe0279f40d
Nikolay Cherniak <nikolay.cherniak@ardas.biz>
2019-05-28 17:06:41 +0300

    move permissions for device in class device

--- src/IdentityServiceClient/IdentityServiceClient/Const.cs
+++ src/IdentityServiceClient/IdentityServiceClient/Const.cs
@@ -5,6 +5,19 @@
         public class Permissions
         {
             public const string RoleClaimType = "extension_Group";
+
+            public class Device
+            {
+                public readonly static string PermissionForCreate = "xyzies.devicemanagment.device.create";
+                public readonly static string PermissionForUpdate = "xyzies.devicemanagment.device.update";
+                public readonly static string PermissionForRead = "xyzies.devicemanagment.device.read";
+                public readonly static string PermissionForDelete = "xyzies.devicemanagment.device.delete";
+
+                public readonly static string AdminPermissionForCreate = "xyzies.devicemanagment.device.create.admin";
+                public readonly static string AdminPermissionForUpdate = "xyzies.devicemanagment.device.update.admin";
+                public readonly static string AdminPermissionForRead = "xyzies.devicemanagment.device.read.admin";
+                public readonly static string AdminPermissionForDelete = "xyzies.devicemanagment.device.delete.admin";
+            }
         }
         public class IndentityApi
         {
@@ -22,16 +35,6 @@
             public const string Full = "xyzies.sso.identity.full";
             public const string Edit = "xyzies.sso.identity.edit";
             public const string Read = "xyzies.sso.identity.read";
-
-            public readonly static string PermissionForCreate = "xyzies.devicemanagment.device.create";
-            public readonly static string PermissionForUpdate = "xyzies.devicemanagment.device.update";
-            public readonly static string PermissionForRead = "xyzies.devicemanagment.device.read";
-            public readonly static string PermissionForDelete = "xyzies.devicemanagment.device.delete";
-
-            public readonly static string AdminPermissionForCreate = "xyzies.devicemanagment.device.create.admin";
-            public readonly static string AdminPermissionForUpdate = "xyzies.devicemanagment.device.update.admin";
-            public readonly static string AdminPermissionForRead = "xyzies.devicemanagment.device.read.admin";
-            public readonly static string AdminPermissionForDelete = "xyzies.devicemanagment.device.delete.admin";
         }
 
     }
