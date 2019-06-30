f1428301baf39ddd5a92bd5b6b787efa4eedf591
Yurii.Kharlamov <yurij.kharlamov@ardas.dp.ua>
2019-06-30 15:40:44 +0300

    Add logging

--- src/IdentityServiceClient/IdentityServiceClient/Filters/AccessFilter.cs
+++ src/IdentityServiceClient/IdentityServiceClient/Filters/AccessFilter.cs
@@ -8,52 +8,60 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace IdentityServiceClient.Filters
 {
     public class AccessFilter : Attribute, IAsyncActionFilter
     {
+        private readonly ILogger<AccessFilter> _logger = null;
         public string[] Scopes { get; set; }
         public IIdentityManager _manager;
         
-        public AccessFilter(params string[] scopes)
+        public AccessFilter(ILogger<AccessFilter> logger,
+            params string[] scopes)
         {
-            Scopes = scopes;
+            Scopes = scopes ??
+                throw new ArgumentNullException(nameof(scopes));
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(scopes));
         }
 
 
         public void OnActionExecuted(ActionExecutedContext context)
         {
         }
-        
+
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            Console.WriteLine($"Starting process to validate and verify access token.");
             _manager = context.HttpContext.RequestServices.GetService<IIdentityManager>();
             var bearerToken = (context.HttpContext.Request.Headers[Const.Auth.AuthHeader]).ToString();
-            var hasPermissionResultList = new List<bool>();
-            if (!string.IsNullOrEmpty(bearerToken))
+            if (string.IsNullOrEmpty(bearerToken))
             {
-                bearerToken = bearerToken.Substring(Const.Auth.BearerToken.Length);
-                var handler = new JwtSecurityTokenHandler();
-                var tokenS = handler.ReadJwtToken(bearerToken);
-                var role = tokenS.Claims.FirstOrDefault(claim => claim.Type == Const.Permissions.RoleClaimType)?.Value;
-                foreach (var scope in Scopes)
+                Console.WriteLine($"Access token is missing.");
+                context.Result = new ContentResult { StatusCode = 401 };
+            }
+
+            bearerToken = bearerToken.Substring(Const.Auth.BearerToken.Length);
+            var handler = new JwtSecurityTokenHandler();
+            var tokenS = handler.ReadJwtToken(bearerToken);
+            var role = tokenS.Claims.FirstOrDefault(claim => claim.Type == Const.Permissions.RoleClaimType)?.Value;
+
+            foreach (var scope in Scopes)
+            {
+                var scopes = scope.Split(',');
+                var hashPermission = await _manager.HasPermission(role, scopes);
+                if (!hashPermission)
                 {
-                    var scopesForOneRole = scope.Split(',');
-                    hasPermissionResultList.Add(await _manager.HasPermission(role, scopesForOneRole));
-                }
-                if (hasPermissionResultList.All(x => !x)) 
-                {
+                    Console.WriteLine($"Not enough permissions");
                     context.Result = new ContentResult { StatusCode = 403 };
                 }
                 else
                 {
+                    Console.WriteLine($"Access token is verified successfully");
                     await next();
                 }
-            }
-            else
-            {
-                context.Result = new ContentResult { StatusCode = 403 };
             }
         }
     }
