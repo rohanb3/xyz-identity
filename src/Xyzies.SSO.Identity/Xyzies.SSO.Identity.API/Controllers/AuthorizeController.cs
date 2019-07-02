84b8258974857badab756ec7ee9a2f43826628e1
yuriipl <yurii.platonov@ardas.biz>
2019-07-02 11:26:15 +0300

    for rebuild

--- src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/AuthorizeController.cs
+++ src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.API/Controllers/AuthorizeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
