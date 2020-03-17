6cd5a998931c0345262e7bc0c8e3e7d43754fb7a
Andrey Matushenko <andrii.matiushenko@ardas.dp.ua>
2020-03-17 10:57:58 +0200

    Hotfix: Update users model to add company id

--- src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.Service/Models/User/UserBaseModel.cs
+++ src/Xyzies.SSO.Identity/Xyzies.SSO.Identity.Service/Models/User/UserBaseModel.cs
@@ -9,5 +9,6 @@ namespace Xyzies.SSO.Identity.Services.Models.User
         public string ObjectId { get; set; }
 
         public string UserName { get; set; }
+        public string CompanyId { get; set; }
     }
 }
